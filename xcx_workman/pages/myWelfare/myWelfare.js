72ed914e1c1c37601e91acb5b42716f4add006e0
薄浩宇 <123456>
2020-07-13 10:32:54 +0800

    1

--- xcx_workman/pages/myWelfare/myWelfare.js
+++ xcx_workman/pages/myWelfare/myWelfare.js
@@ -11,6 +11,15 @@ Page({
   data: {
     viewUrl: api.viewUrl,
     isShowConfirm:false,
+    GyTypeid:1,
+    tid:'',
+    type:[{
+      tid:1,
+      name:'我发布的公益'
+    },{
+      tid:2,
+      name:'我参加的公益'
+    }],
     gongyilist:[{
       id:1,
       title:'七月敬老活动',
@@ -22,27 +31,47 @@ Page({
       content:'关爱老人，关爱留守儿童',
       time:'2020-7-20' 
     }],
+    pageNo:1,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getActivity()
   },
-  //切换
-  changeType: function(e) {
+  // 切换类型
+  changetype: function(e) {
     var that = this;
     var id = e.currentTarget.dataset.id
     that.setData({
-      needsTypeid: id
+      GyTypeid: id
     })
-    // this.getmyEmploy()
+    that.getActivity()
+  },
+  // 获取公益活动列表
+  getActivity:function(){
+    var that = this
+    var data = {
+      wxUserId:app.globalData.wxid,
+      pageNo:that.data.pageNo,
+      pageSize:10
+    }
+    qingqiu.get("getActivityList",data,function(res){
+      console.log('我的公益活动列表',res)
+      if(res.success == true){
+        that.setData({
+          gongyilist:res.result.records
+        })
+      }
+    })
   },
   // 公益详情
-  WelfareDetail:function(){
+  WelfareDetail:function(e){
+    var list=e.currentTarget.dataset.list
+    var list1 = JSON.stringify(list)
     wx.navigateTo({
-      url: '../WelfareDetail/WelfareDetail',
+      url: '../WelfareDetail/WelfareDetail?obj='+list1,
     })
   },
 })
