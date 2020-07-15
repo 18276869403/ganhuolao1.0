3ebe27d14f7ab2da276b7b5a07e19f20c4b8463d
薄浩宇 <123456>
2020-07-15 17:07:59 +0800

    1

--- xcx_workman/pages/myGoods/myGoods.js
+++ xcx_workman/pages/myGoods/myGoods.js
@@ -59,6 +59,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    this.setData({
+      isLastPage:false,
+      pageNo:1,
+      goodsLists:[]
+    })
     wx.showShareMenu({
       withShareTicket: true
     })
@@ -66,11 +71,6 @@ Page({
   },
   // 下拉刷新
   onPullDownRefresh: function () {
-    this.setData({
-      isLastPage:false,
-      pageNo:1,
-      goodsLists:[]
-    })
     this.onLoad()
     setTimeout(() => {
       wx.stopPullDownRefresh()
