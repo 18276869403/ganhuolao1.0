25519f4c9c3c9f9b490fe0e8b5bc8186699791f4
邓星玉 <1336797684@qq.com>
2020-07-28 10:02:08 +0800

    1

--- xcx_workman/pages/myRecruitment/myRecruitment.js
+++ xcx_workman/pages/myRecruitment/myRecruitment.js
@@ -126,32 +126,5 @@ Page({
       this.FindWorklist()
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  
 })
