505a51731a372dee00f9039edacf3f0a8d21c88f
bai <bai211425401@126.com>
2020-08-07 15:24:58 +0800

    1

--- xcx_workman/pages/convenienceDetails/convenienceDetails.js
+++ xcx_workman/pages/convenienceDetails/convenienceDetails.js
@@ -9,7 +9,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    convenience:{}
+    iconUrl:api.iconUrl,
+    convenience:{},
+    tupian:[]
   },
 
   /**
@@ -22,25 +24,62 @@ Page({
       console.log(res)
       if(res.success == true){
         if(res.result != null){
+          if(res.result.pic.indexOf(',') != -1){
+            that.data.tupian = res.result.pic.split(',')
+          }else{
+            that.data.tupian.push(res.result.pic)
+          }
           that.setData({
-            convenience:res.result
+            convenience:res.result,
+            tupian:that.data.tupian
           })
         }
       }
     })
   },
 
+  /**
+   * 图片放大
+   * @param {*} e 
+   */
+  tupian:function(e){
+    var url = e.currentTarget.dataset.url
+    wx.previewImage({
+      current:url,
+      urls: [url],
+    })
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.chushihuabyid(1)
+    // this.chushihuabyid(1)
     // 通过id加载数据
     if(options != undefined){
       if(options.id != undefined){
         this.chushihuabyid(options.id)
+      }else{
+        wx.showToast({
+          title: '资源不存在',
+          icon:'none'
+        })
+        setTimeout(function(){
+          wx.navigateBack({
+            delta:1
+          })
+        })
       }
+    }else{
+      wx.showToast({
+        title: '资源不存在',
+        icon:'none'
+      })
+      setTimeout(function(){
+        wx.navigateBack({
+          delta:1
+        })
+      })
     }
   },
 
